99ec4478afb83b9237984dac3c2abdb61e35910c
Chad Autry <chad.autry@gmail.com>
2018-11-11 06:04:15 -0600

    Create entrypoint.sh
    Add a shell script to act as the entry point and perform required setup before executing ansible

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# render a template configuration file
+# expand variables + preserve formatting
+render_template() {
+  eval "echo \"$(cat $1)\""
+}
+
+gcloud auth activate-service-account --key-file=$GCP_SERVICE_ACCOUNT_FILE
+gcloud config set project $GCP_PROJECT
+
+#template out the inventory file since it requires the project
+render_template /usr/var/ansible/inventory.gcp.yml.template > /usr/var/ansible/hosts/inventory.gcp.yml
+
+
+exec "$@"
