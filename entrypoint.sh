380935dd25c0c76e15e03aef26a042356d6eac6b
Chad Autry <chad.autry@gmail.com>
2018-11-20 11:46:28 -0600

    Update entrypoint.sh
    Set project first

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ render_template() {
   eval "echo \"$(cat $1)\""
 }
 
-gcloud auth activate-service-account --key-file=$GCP_SERVICE_ACCOUNT_FILE
 gcloud config set project $GCP_PROJECT
+gcloud auth activate-service-account --key-file=$GCP_SERVICE_ACCOUNT_FILE
 
 #template out the inventory file since it requires the project
 render_template /usr/var/ansible/inventory.gcp.yml.template > /etc/ansible/hosts/inventory.gcp.yml
